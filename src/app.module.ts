2b39b7e4b3a349f587fb44df35e955026119233a
Cristhian Santos <cristhianfelipe0@gmail.com>
2025-02-20 13:25:19 -0300

    ✨ feat: add cron job service and integrate nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { GameModule } from './game/game.module';
 import { PresentationModule } from './presentation/presentation.module';
 import { RoomsModule } from './rooms/rooms.module';
+import { CronJobService } from './shared/cron-job.service';
 
 @Module({
-  imports: [RoomsModule, GameModule, PresentationModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    RoomsModule,
+    GameModule,
+    PresentationModule,
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [CronJobService],
 })
 export class AppModule {}
