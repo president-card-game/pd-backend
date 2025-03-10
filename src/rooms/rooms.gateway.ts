3fc51da2e2cac616755b3ca3d2701e94235eb44c
Jorge <jorgevitorp9@gmail.com>
2025-03-10 11:12:06 -0300

    ♻️ refactor(rooms): simplify room and user management logic

--- src/rooms/rooms.gateway.ts
+++ src/rooms/rooms.gateway.ts
@@ -19,39 +19,35 @@ export class RoomsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   createRoom(@MessageBody() roomName: string, @ConnectedSocket() client: Socket) {
     const roomId = this.roomsService.createRoom(roomName);
     client.emit('roomCreated', { id: roomId, name: roomName });
-    console.log(`${client.id} create the room ${roomId}`);
   }
 
   @SubscribeMessage('joinRoom')
-  async joinRoom(@MessageBody() data: { roomId: string }, @ConnectedSocket() client: Socket) {
-    const room = this.roomsService.addUserToRoom({ roomId: data.roomId, userId: client.id });
+  async joinRoom(@MessageBody() roomId: string, @ConnectedSocket() client: Socket) {
+    const room = this.roomsService.addUserToRoom({ roomId, userId: client.id });
     if (!room) {
-      client.emit('errorMessage', `Sala ${data.roomId} não existe`);
+      client.emit('errorMessage', `Sala ${roomId} não existe`);
       return;
     }
 
-    await client.join(data.roomId);
+    await client.join(roomId);
     this.roomsUpdate();
-    console.log(`Usuário ${client.id} entrou na sala ${room.name}`);
   }
 
   @SubscribeMessage('leaveRoom')
-  async leaveRoom(@MessageBody() roomId: string, @ConnectedSocket() client: Socket) {
-    const room = this.roomsService.removeUserFromRoom(roomId, client.id)!;
+  async leaveRoom(@ConnectedSocket() client: Socket) {
+    const room = this.roomsService.removeUserFromRoom(client.id)!;
 
     if (!room) return;
 
-    await client.leave(roomId);
-    this.server.to(roomId).emit('roomUsers', room.users);
+    await client.leave(room.id);
+    this.server.to(room.id).emit('roomUsers', room.users);
     this.roomsUpdate();
-
-    console.log(`Usuário ${client.id} saiu da sala ${room.name}`);
   }
 
   @SubscribeMessage('toggleUserReady')
-  toggleUserReady(@ConnectedSocket() client: Socket, @MessageBody() data: { roomId: string; isReady: boolean }) {
-    const room = this.roomsService.toggleUserReady({ roomId: data.roomId, userId: client.id, isReady: data.isReady });
-    this.server.to(data.roomId).emit('roomUsers', room.users);
+  toggleUserReady(@ConnectedSocket() client: Socket) {
+    const room = this.roomsService.toggleUserReady(client.id);
+    this.server.to(room.id).emit('roomUsers', room.users);
   }
 
   @SubscribeMessage('getRooms')
@@ -72,12 +68,10 @@ export class RoomsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   handleConnection(client: Socket) {
-    console.log(`Client ${client.id} connected to rooms gateway`);
-    this.roomsUpdate();
+    client.emit('roomsUpdate', this.roomsService.getRooms());
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Client ${client.id} disconnected from rooms gateway`);
     const room = this.roomsService.removeUserFromRoomWhenDisconnected(client.id);
 
     if (!room) return;
